a808b7a68f1cc34caed467631e2f521d87cd4274
kuidaoring <kuijob@gmail.com>
2023-10-19 01:03:39 +0900

    サイドバーに未完了のタスク数を表示

--- renderer/components/Sidebar.tsx
+++ renderer/components/Sidebar.tsx
@@ -12,35 +12,73 @@ import type { MenuProps } from "antd";
 import styles from "../styles/Sidebar.module.css";
 import { useState } from "react";
 import Link from "next/link";
-
-const menuItems = [
-  {
-    label: <Link href="/list/today">今日のタスク</Link>,
-    icon: <RocketOutlined />,
-    key: "today",
-  },
-  {
-    label: "予定日別タスク",
-    icon: <ScheduleOutlined />,
-  },
-  {
-    label: "期限日別タスク",
-    icon: <CalendarOutlined />,
-  },
-  {
-    label: <Link href="/list/all">全てのタスク</Link>,
-    icon: <CheckSquareOutlined />,
-    key: "all",
-  },
-];
+import { useAtomValue } from "jotai";
+import { RawTaskListAtom, TaskListAtom } from "../atoms/atoms";
+import dayjs from "dayjs";
 
 const Sidebar = ({ className, selected }) => {
   const [colapsed, setColapsed] = useState(false);
+  const taskList = useAtomValue(RawTaskListAtom);
+
+  const todaysNotCompletedTaskCount = taskList.filter(
+    (task) =>
+      !task.completed &&
+      task.planDate &&
+      dayjs(task.planDate).isSame(dayjs(), "day")
+  ).length;
+
+  const allNotCompletedTaskCount = taskList.filter(
+    (task) => !task.completed
+  ).length;
+
+  const menuItems = [
+    {
+      label: (
+        <Link href="/list/today" className={styles.menuLink}>
+          <span>今日のタスク</span>
+          {todaysNotCompletedTaskCount > 0 && (
+            <span className={styles.taskCountTag}>
+              {todaysNotCompletedTaskCount}
+            </span>
+          )}
+        </Link>
+      ),
+      title: "今日のタスク",
+      icon: <RocketOutlined />,
+      key: "today",
+    },
+    {
+      label: "予定日別タスク",
+      titile: "予定日別タスク",
+      icon: <ScheduleOutlined />,
+    },
+    {
+      label: "期限日別タスク",
+      title: "期限日別タスク",
+      icon: <CalendarOutlined />,
+    },
+    {
+      label: (
+        <Link href="/list/all" className={styles.menuLink}>
+          <span>全てのタスク</span>
+          {allNotCompletedTaskCount > 0 && (
+            <span className={styles.taskCountTag}>
+              {allNotCompletedTaskCount}
+            </span>
+          )}
+        </Link>
+      ),
+      title: "全てのタスク",
+      icon: <CheckSquareOutlined />,
+      key: "all",
+    },
+  ];
 
   return (
     <Sider
       className={className}
       style={{ backgroundColor: "rgb(244, 244, 245)" }}
+      width={colapsed ? 100 : 250}
       collapsed={colapsed}
     >
       <div className={styles.container}>
@@ -49,7 +87,7 @@ const Sidebar = ({ className, selected }) => {
           items={menuItems as MenuProps["items"]}
           defaultSelectedKeys={[selected]}
           style={{ borderRight: "none" }}
-        ></Menu>
+        />
         <div className={styles.footer}>
           <Button
             type="text"
